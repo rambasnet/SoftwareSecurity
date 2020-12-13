a9bd4cc9433767a0e03eaa06d89136ff230598b5
Ram Basnet <rambasnet@gmail.com>
2020-12-13 14:27:35 -0500

    updated stuff

--- booksrc/stack_overflow_stdio.cpp
+++ booksrc/stack_overflow_stdio.cpp
@@ -10,27 +10,33 @@ using namespace std;
 
 using namespace std;
 
+void give_shell(){
+    // Set the gid to the effective gid
+    // this prevents /bin/sh from dropping the privileges
+    gid_t gid = getegid();
+    setresgid(gid, gid, gid);
+    system("/bin/sh -i");
+}
+
 char * mgets(char *dst) {
     char *ptr = dst;
-	char ch; 
-    // int ch;
+    char ch; 
 	/* skip leading white spaces */ 
-    // ch = getchar()
-	while (cin.get(ch) && ch == ' ' or ch == '\t') 
-		; 
- 
-	if ((ch == '\n') or (ch == EOF)) { 
-		*ptr = '\0';
-		return dst; 
-	} 
-	else
-		*ptr = ch;
- 
-	/* now read the rest until \n or EOF */ 
-	while (ch = getchar()) {
+    while (cin.get(ch) && ch == ' ' or ch == '\t') 
+        ; 
+
+    if ((ch == '\n') or (ch == EOF)) { 
+        *ptr = '\0';
+        return dst; 
+    } 
+    else
+        *ptr = ch;
+
+    /* now read the rest until \n or EOF */ 
+    while (ch = getchar()) {
         if (ch == '\n' or ch == EOF) break;
-		*(++ptr) = ch; 
-	} 
+        *(++ptr) = ch; 
+    } 
     *(++ptr) = 0;
     return dst;
 }
@@ -39,17 +45,13 @@ void bad() {
     char buffer[BUFSIZE];
     printf("buffer is at %p\n", buffer);
     printf("Enter some data: ");
-    mgets(buffer);
-    //copydata(buffer, data);
+    mgets(buffer); // similar to C's gets; depricated
+    //gets(buffer);
     cout << "buffer contains: " << buffer << endl;
     printf("size of buffer = %d and length is %d\n", sizeof(buffer), strlen(buffer));
 }
 
 int main(int argc, char *argv[]) {
-    // Set the gid to the effective gid
-    // this prevents /bin/sh from dropping the privileges
-    //uid_t uid = getuid();
-    //setresgid(uid, uid, uid);
     bad();
     cout << "Good bye!\n";
     return 0;
