f3bec3ef4eea3691327d3d5d46696e334162d2b6
Ram Basnet <rambasnet@gmail.com>
2020-12-17 00:27:54 -0700

    updated exploiting with env

--- demos/stack_overflow/getenvaddr.cpp
+++ demos/stack_overflow/getenvaddr.cpp
@@ -3,16 +3,16 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char *ptr;
+    char *ptr;
 
-	if(argc < 3) {
-		printf("Usage: %s <env variable name> <target program name>\n", argv[0]);
-	}
-	else {
-		ptr = getenv(argv[1]); /* get env var location */
-		int diff = (strlen(argv[0]) - strlen(argv[2]))*2;
-		ptr += diff; /* adjust for program name */
-		printf("%s will be at %p with reference to %s\n", argv[1], ptr, argv[2]);
-	}
-	return 0;
+    if(argc < 3) {
+        printf("Usage: %s <env variable name> <target program name>\n", argv[0]);
+    }
+    else {
+        ptr = getenv(argv[1]); /* get env var location */
+        int diff = (strlen(argv[0]) - strlen(argv[2]))*2;
+        ptr += diff; /* adjust for program name */
+        printf("%s will be at %p with reference to %s\n", argv[1], ptr, argv[2]);
+    }
+    return 0;
 }
