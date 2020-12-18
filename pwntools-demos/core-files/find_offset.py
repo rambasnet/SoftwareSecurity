3f1fc1897e66d5cdba3941e6498f0264a81e2048
Ram Basnet <rambasnet@gmail.com>
2020-12-18 14:12:27 -0700

    added Pwntools tutorial

--- pwntools-demos/core-files/find_offset.py
+++ pwntools-demos/core-files/find_offset.py
@@ -1,3 +1,18 @@
+#! /usr/bin/env python3
+
 from pwn import *
 
-io = process('./so_stdio.exe')
+target_program = "./vuln_cpp.exe"
+
+elf = ELF(target_program)
+
+p = process(target_program)
+
+# https://docs.pwntools.com/en/stable/util/cyclic.html
+# create 200 characters cyclic length
+p.sendline(cyclic(200, n=8))
+p.wait()
+
+core = p.corefile
+offset = cyclic_find(core.read(core.esp, 8), n=8)
+print(f'offset = {offset}')
