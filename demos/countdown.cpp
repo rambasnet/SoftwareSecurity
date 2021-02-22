12da5d8cd5ca5c43c881d704eccfee74cf288f33
Ram Basnet <rambasnet@gmail.com>
2021-02-21 23:15:43 -0700

    update ELF

---
+++ demos/countdown.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unistd.h>
+//unsigned int sleep(unsigned int seconds);
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "Launching of Perseverence Rover count down...\n";
+    sleep(1);
+    system("clear");
+    for(int i=10; i>=1; i--) {
+        cout << i << endl;
+        sleep(1);
+        system("clear");
+    }
+    cout << "Blast Off!\n";
+    sleep(1);
+    system("clear");
+    return 0;
+}
