a8d3f7c10dc9d82011b3a1a1e115915f7eeada5c
Ram Basnet <rambasnet@gmail.com>
2021-03-03 11:39:18 -0700

    add gdb-demo.cpp

---
+++ demos/gdb-demo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+void countDown(int n) {
+    printf("Countdown from %d\n", n);
+    for(int i=n; i>0; i--) {
+        cout << i << endl;
+    }
+    cout << "Blast off!" << endl;
+    return;
+}
+
+int main(int argc, char **argv) {
+    countDown(10);
+    return 0;
+}
