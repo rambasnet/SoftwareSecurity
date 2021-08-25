ff60317fb766c6bd9431d2fb194e9bc4b127917d
Ram Basnet <rambasnet@gmail.com>
2021-08-25 12:36:50 -0600

    add heapoverflow lab

---
+++ labs/heap_overflow/secret.cpp
@@ -0,0 +1,48 @@
+// heap_overflow lab
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <unistd.h> //getuid()
+#include <sys/types.h> // getuid()
+#include <filesystem>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+void usage(char *prog_name, char *filename) {
+   printf("Usage: %s <secret to add to %s>\n", prog_name, filename);
+   exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    int userid;
+    char *secret, *secret_file;
+    ofstream fout;
+    
+    secret = new char[200];
+    secret_file = new char[20];
+
+
+    strcpy(secret_file, "/var/secret");
+
+    if (argc < 2)
+        usage(argv[0], secret_file);
+    
+    strcpy(secret, argv[1]);
+
+    userid = getuid();
+    fout.open(secret_file, ios_base::app); // append mode
+    if (!fout) {
+        cerr << "Error while opening file\n";
+        cerr << "Make sure " << argv[0] << " has r/w permission to /var folder\n";
+        exit(1);
+    }
+    fout << userid << "\n" << secret << endl;
+    fs::permissions(secret_file, fs::perms::group_read|fs::perms::others_read, fs::perm_options::remove);
+    fout.close();
+    cout << "Secret saved.\n";
+    delete[] secret;
+    delete[] secret_file;
+}
