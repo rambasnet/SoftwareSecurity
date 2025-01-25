bd79005ed326e1e3dfc2834ea833ce6ac3ae6621
Ram Basnet <rambasnet@gmail.com>
2025-01-25 00:18:53 -0500

    add x64 overflow

---
+++ demos/stack_overflow/x64/stack.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const int MAX_DATA = 200;
+
+void vulnerable(char *str) {
+	char buffer[100];
+	strcpy(buffer, str);
+	cout << buffer << endl;
+}
+
+int main(int argc, char *argv[]) {
+	char data[MAX_DATA];
+	//file_name = strcpy(file_name, argv[1]
+	FILE *fin;
+	fin = fopen(argv[1], "r");
+	fread(data, sizeof(char), MAX_DATA, fin);
+	vulnerable(data);
+	cout << "All done..." << endl;
+	return 0;
+}
+
