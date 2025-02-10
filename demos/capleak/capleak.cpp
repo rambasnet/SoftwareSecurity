2470287a57a167f101ecff3daade070322dba18a
Ram Basnet <rambasnet@gmail.com>
2025-02-10 18:29:51 +0000

    update capleak; print_pwd

--- demos/capleak/capleak.cpp
+++ demos/capleak/capleak.cpp
@@ -6,23 +6,25 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     int fd;
     char *v[2];
-    
-    fd = open("/etc/xyz", O_RDWR | O_APPEND | O_CREAT); 
-    if (fd == -1) {
+
+    fd = open("/etc/xyz", O_RDWR | O_APPEND | O_CREAT);
+    if (fd == -1)
+    {
         printf("Failed to open /etc/xyz\n");
         exit(1);
     }
     // print out the file descriptor value
     printf("fd is %d\n", fd);
-    // FIXME - close the file
-    // close(fd);
-    // downgrade the privilege my making the effective uid the same as the real uid
+    // FIXME - close the file descriptor
+    // downgrade the privilege mb making the effective uid the same as the real uid
     setuid(getuid());
-    // execute /bin/sh
-    v[0] = "/bin/sh"; v[1] = 0;
+    // execute /bin/sh using execve
+    v[0] = "/bin/sh";
+    v[1] = 0;
     execve(v[0], v, 0);
     return 0;
 }
--- demos/envvars/print_pwd.cpp
+++ demos/envvars/print_pwd.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-    char buffer[100]; // this may be vulnerable!
+int main()
+{
+    char buffer[100]; // this is vulnerable to stack-overflow!
     char *ptr;
     ptr = getenv("PWD");
-    if (ptr != NULL){
+    if (ptr != NULL)
+    {
         sprintf(buffer, "Current working directory is: %s", ptr);
-        printf("%s\n", buffer);
+        printf(buffer); // this is vulnerable to printf-injection!
     }
     return 0;
 }
