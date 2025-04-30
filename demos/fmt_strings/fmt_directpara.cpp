6e314744cd8f576f826ac23249b2dbf9ff38e6c7
Ram Basnet <rambasnet@gmail.com>
2025-04-30 16:41:50 -0400

    update Format Strings

--- demos/fmt_strings/fmt_directpara.cpp
+++ demos/fmt_strings/fmt_directpara.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("7th: %7$d, 4th: %4$05d\n", 10, 20, 30, 40, 50, 60, 70, 80);
+    printf("7th: %7$d, 4th: %4$05d which = %d next? = %d \n", 10, 20, 30, 40, 50, 60, 70, 80);
     return 0;
 }
